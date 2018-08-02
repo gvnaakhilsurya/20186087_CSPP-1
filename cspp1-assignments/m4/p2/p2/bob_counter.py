595e978dfe567539e69f80987fec60541bf45ffd
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-02 21:39:34 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1-assignments/m4/p2/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/p2/bob_counter.py
@@ -12,11 +12,11 @@ Write a program that prints the number of times
  the sub string in the main string.
 '''
 STR_INPUT = input()
-NUM_LEN = len(str_input)
+NUM_LEN = len(STR_INPUT)
 i = 0
 C = 0
 for i in range(0, NUM_LEN-2):
-    if(str_input[i] == 'b' and str_input[i+1] == 'o' and str_input[i+2] == 'b'):
+    if(STR_INPUT[i] == 'b' and STR_INPUT[i+1] == 'o' and STR_INPUT[i+2] == 'b'):
         C = C+1
 print(C)
 
