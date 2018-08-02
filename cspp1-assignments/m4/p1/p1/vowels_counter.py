96336f8b53f408062410c37d777bfbcdd15ed98e
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-02 17:18:26 +0530

    " -> 0 of 4 passed. pylint: 2.00/10 "

---
+++ cspp1-assignments/m4/p1/p1/vowels_counter.py
@@ -0,0 +1,16 @@
+'''
+Assume s is a string of lower case characters.
+Write a program that counts up the number of vowels contained in the string s
+Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example,
+if s = 'azcbobobegghakl', your program should print:
+Number of vowels: 5
+@author :gvnaakhil
+'''
+def main():
+    s = input()
+    # the input string is in s
+    # remove pass and start your code here
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ cspp1-pratice/m3/w_sum_1_end.py
@@ -0,0 +1,18 @@
+'''
+@author : gvnaakhilsurya
+Write a piece of Python code that prints out a while loop
+that sum of the values 1 through end,
+inclusive.end is a variable that
+we define
+foryou.
+So,forexample,
+if we define end to be 6,
+your code should print out the result:
+21
+which is 1 + 2 + 3 + 4 + 5 + 6.
+'''
+END = int(input("Enter the end int value :"))
+SUM = 0
+for i in range(1, END+1, 1):
+    SUM += i
+print(SUM)
