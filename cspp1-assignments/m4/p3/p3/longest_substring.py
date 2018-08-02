4b9fd9b6babbd72557d96d069150541a33ea698d
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-02 23:59:29 +0530

    " -> 5 of 5 passed. pylint: 8.46/10 "

--- cspp1-assignments/m4/p3/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/p3/longest_substring.py
@@ -1,4 +1,7 @@
-
+'''
+@author : gvnakhilsurya
+longest sequence in a string
+'''
 def main():
     # the input string is in s
     # remove pass and start your code here
@@ -9,10 +12,10 @@ def main():
     temp_str = ""
     for i in range(len(input_str)-1):
         sub_str = input_str[i]
-        while i+1 < len(input_str) and input_str[i] <= input_str[i+1]:
-            i=i+1
+        while i + 1 < len(input_str) and input_str[i] <= input_str[i+1]:
+            i = i + 1 
             sub_str = sub_str + input_str[i]
-        if len(sub_str)>len(temp_str):
+        if len(sub_str) > len(temp_str):
             temp_str = sub_str
     print(temp_str)
 
