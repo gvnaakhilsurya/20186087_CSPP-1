00c870a180a0cde0bd7433bc8ef4a66b7c4e6566
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-04 23:19:36 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- cspp1-assignments/m5/p3/p3/square_root_bisection.py
+++ cspp1-assignments/m5/p3/p3/square_root_bisection.py
@@ -1,6 +1,6 @@
 '''
 # using approximation method
-
+@author:gvnaakhilsurya
 # testcase 1
 # input: 25
 # output: 4.999999999999998
@@ -11,23 +11,22 @@
 '''
 
 def main():
-	'''
-	# epsilon and step are initialized
-	# don't change these values
-	'''
-	X_NUM = int(input())
-	EPSILON = 0.01
-	LOWER = 0.0
-	HIGHER = X_NUM
-	BI_VAL = (HIGHER + LOWER)/2.0
-	while abs(BI_VAL**2 - X_NUM) > EPSILON:
-	    if BI_VAL**2 < X_NUM:
-	        LOWER = BI_VAL
-	    else:
-	        HIGHER = BI_VAL
-	    BI_VAL = (HIGHER + LOWER)/2.0
-	print(BI_VAL)
+    '''
+    # epsilon and step are initialized
+    # don't change these values
+    '''
+    x_num = int(input())
+    epsilon = 0.01
+    lower = 0.0
+    higher = x_num
+    bi_val = (higher + lower)/2.0
+    while abs(bi_val**2 - x_num) > epsilon:
+        if bi_val**2 < x_num:
+            lower = bi_val
+        else:
+            higher = bi_val
+        bi_val = (higher + lower)/2.0
+    print(bi_val)
 
-
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
