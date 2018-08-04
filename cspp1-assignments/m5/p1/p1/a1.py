e20438ee71759339953bdbd117b8f023ba114cad
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-04 23:13:23 +0530

    " -> 3 of 3 passed. pylint: -6.43/10 "

---
+++ cspp1-assignments/m5/p1/p1/a1.py
@@ -0,0 +1,12 @@
+X_NUM = int(input())
+EPSILON = 0.01
+LOWER = 0.0
+HIGHER = X_NUM
+BI_VAL = (HIGHER + LOWER)/2.0
+while abs(BI_VAL**2 - X_NUM) > EPSILON:
+    if BI_VAL**2 < X_NUM:
+        LOWER = BI_VAL
+    else:
+        HIGHER = BI_VAL
+    BI_VAL = (HIGHER + LOWER)/2.0
+print(BI_VAL)
--- cspp1-assignments/m5/p3/p3/square_root_bisection.py
+++ cspp1-assignments/m5/p3/p3/square_root_bisection.py
@@ -1,4 +1,4 @@
-# Write a python program to find the square root of the given number
+'''
 # using approximation method
 
 # testcase 1
@@ -8,14 +8,26 @@
 # testcase 2
 # input: 49
 # output: 6.999999999999991
+'''
 
 def main():
-	s = raw_input()
+	'''
 	# epsilon and step are initialized
 	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
+	'''
+	X_NUM = int(input())
+	EPSILON = 0.01
+	LOWER = 0.0
+	HIGHER = X_NUM
+	BI_VAL = (HIGHER + LOWER)/2.0
+	while abs(BI_VAL**2 - X_NUM) > EPSILON:
+	    if BI_VAL**2 < X_NUM:
+	        LOWER = BI_VAL
+	    else:
+	        HIGHER = BI_VAL
+	    BI_VAL = (HIGHER + LOWER)/2.0
+	print(BI_VAL)
+
 
 if __name__== "__main__":
 	main()
