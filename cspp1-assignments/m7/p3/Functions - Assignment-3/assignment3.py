caf189f9f6a275a40eaab1ebaa8a2f939487e9a8
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-07 00:28:52 +0530

    " -> 10 of 10 passed. pylint: 8.80/10 "

--- cspp1-assignments/m7/p3/Functions - Assignment-3/assignment3.py
+++ cspp1-assignments/m7/p3/Functions - Assignment-3/assignment3.py
@@ -25,8 +25,6 @@ def payingDebtOff_inAYear(balance_in, annual_interestrate):
         else:
             break
     return str(round(mon_payrate, 2))
-
-
 def main():
     data = input()
     data = data.split(' ')
