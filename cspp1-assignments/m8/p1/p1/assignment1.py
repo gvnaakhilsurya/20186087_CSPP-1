39e4dbc49df6b26e43cf1c2264964253deb5a277
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-07 19:31:41 +0530

    " -> 10 of 10 passed. pylint: 0.00/10 "

--- cspp1-assignments/m8/p1/p1/assignment1.py
+++ cspp1-assignments/m8/p1/p1/assignment1.py
@@ -1,5 +1,10 @@
+'''
+
+'''
+#@author:akhilsurya
 # Exercise: Assignment-1
-# Write a Python function, factorial(n), that takes in one number and returns the factorial of given number.
+# Write a Python function, factorial(n), that takes in one
+# number and returns the factorial of given number.
 
 # This function takes in one number and returns one number.
 
