10ed7f3a85735fa7342760f9ff79f62376ef336a
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-11 12:59:11 +0530

    " -> 0 of 3 passed. pylint: 8.82/10 "

--- cspp1-assignments/m12/p1/p1/create_social_network.py
+++ cspp1-assignments/m12/p1/p1/create_social_network.py
@@ -36,8 +36,8 @@ def create_social_network(data):
     #print(data)
     dict_1 = {}
     for i in range(0, len(data), 1):
-        if data[i] not in d:
-            d[data[i]] =data[i+1].split()
+        if data[i] not in dict_1:
+            dict_1[data[i]] =data[i+1].split()
 
     return dict_1
 def main():
