932b170d4e9c1bc3f663cc7892f99f1fc0b08fbd
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-12 19:49:15 +0530

    " -> 6 of 6 passed. pylint: 7.50/10 "

--- cspp1-assignments/m11/p2/p2/assignment2.py
+++ cspp1-assignments/m11/p2/p2/assignment2.py
@@ -3,7 +3,7 @@
 '''
 #Exercise: Assignment-2
 #Implement the updateHand function. Make sure this function has
-# no side effects: i.e., it must not mutate the hand passed in. Before pasting 
+# no side effects: i.e., it must not mutate the hand passed in. Before pasting
 #your function definition here, be sure you've passed the appropriate tests
 # in test_ps4a.py.
 def update_hand(hand, word):
@@ -11,7 +11,7 @@ def update_hand(hand, word):
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
     Has no side effects: does not modify hand.
@@ -21,7 +21,7 @@ def update_hand(hand, word):
     """
     for letter in word:# if letter is in the word then search in hand
         if letter in hand:# if letter is in hand
-            hand[letter] -=1# then decrement the hand i.e all the letters in word
+            hand[letter] -= 1# then decrement the hand i.e all the letters in word
     return hand
 
 def main():
