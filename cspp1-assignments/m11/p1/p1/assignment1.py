03959146e42c1832165c855b972c651153f639e5
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-12 18:51:54 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- cspp1-assignments/m11/p1/p1/assignment1.py
+++ cspp1-assignments/m11/p1/p1/assignment1.py
@@ -11,7 +11,7 @@ SCRABBLE_LETTER_VALUES = {
     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
-def get_word_score(word, n_):
+def get_word_score(word, n_num):
     """
     Returns the score for a word. Assumes the word is a valid word.
     The score for a word is the sum of the points for letters in the
@@ -28,7 +28,7 @@ def get_word_score(word, n_):
         if i in SCRABBLE_LETTER_VALUES:
             sum_points += SCRABBLE_LETTER_VALUES[i]
     sum_points = sum_points * len(word)
-    if len(word) == n_:#n_ is the length of scrabble letters given by user,
+    if len(word) == n_num:#n_num is the length of scrabble letters given by user,
     #if we can arrange a valid word at a time then we will add +50 to it.
         sum_points = sum_points + 50
     return sum_points
