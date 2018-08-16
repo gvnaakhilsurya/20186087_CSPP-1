09f3107024969aa7332fe0c7347e0a250e73bdbd
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-16 19:55:51 +0530

    " -> 0 of 8 passed. pylint: 0/0 "

--- cspp1-pratice/m14/poker/pokerv2.py
+++ cspp1-pratice/m14/poker/pokerv2.py
@@ -113,20 +113,20 @@ def hand_rank(hand):
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
     # max in poker function uses these return values to select the best hand
-    if is_one_pair(hand):
-        return 2
-    elif is_two_pair(hand):
-        return 3
-    elif is_three_a_kind(hand):
-        return 4
-    elif is_straight(hand):
-        return 5
-    elif is_flush(hand):
-        return 6
-    elif is_four_a_kind(hand):
-        return 7
     elif is_straight(hand) and is_flush(hand):
         return 8
+    elif is_four_a_kind(hand):
+        return 7
+    elif is_flush(hand):
+        return 6
+    elif is_straight(hand):
+        return 5
+    elif is_three_a_kind(hand):
+        return 4
+    elif is_two_pair(hand):
+        return 3
+    if is_one_pair(hand):
+        return 2
     else:
         return 0
 
