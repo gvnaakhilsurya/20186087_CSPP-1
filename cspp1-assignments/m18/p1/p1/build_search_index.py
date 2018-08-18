34f980eb71c4b68d17553ff74227dc30d4ab554d
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-18 12:47:53 +0530

    " -> 0 of 1 passed. pylint: 6.75/10 "

--- cspp1-assignments/m18/p1/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/p1/build_search_index.py
@@ -1,5 +1,6 @@
 '''
     Tiny Search Engine - Part 1 - Build a search index
+    
 
     In this programming assingment you are given with some text documents as input.
     Complete the program below to build a search index. Don't worry, it is explained below.
@@ -23,27 +24,56 @@
 
 import re
 def load_stopwords(filename):
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
     stopwords = {}
     with open(filename, 'r') as f_stopwords:
         for line in f_stopwords:
             stopwords[line.strip()] = 0
     return stopwords
 
+
 def word_list(string):
+    '''
+        Change case to lower and split the words using a SPACE
+        Clean up the text by remvoing all the non alphabet characters
+        return a list of words
+    '''
+
     regex = re.compile('[^a-z]')
-    return [regex.sub("", w.strip()) for w in string.lower().split(" ")]
+    clean_words = [regex.sub("", word.strip()) for word in string.lower().split(" ")]
+    return clean_words
+
 
 def build_search_index(docs):
+    '''
+        Process the docs step by step as given below
+    '''
+
+    # initialize a search index (an empty dictionary)
+
+    # iterate through all the docs
+    # keep track of doc_id which is the list index corresponding the document
+    # hint: use enumerate to obtain the list index in the for loop
+
+        # clean up doc and tokenize to words list
+
+        # add or update the words of the doc to the search index
+
+    # return search index
     dict_1 = {}
     STOP_WORD = load_stopwords("stopwords.txt")
-    for index, line in enumerate(docs):
+    for index_dict, line in enumerate(docs):
         LIST_ = remove_stopwords(word_list(line), STOP_WORD)
-        for ele in set(LIST_):
-            if ele in dict_1:
-                dict_1[ele].append((index, LIST_.count(ele)))
-            else:
-                dict_1[ele] = [(index, LIST_.count(ele))]
+    for word in set(LIST_):
+         if word in dict_1:
+                dict_1[word].append((index_dict, LIST_.count(word)))
+         else:
+             dict_1[word] = [(index_dict, LIST_.count(word))]  
+
     return dict_1
+
 
 def remove_stopwords(word, STOP_WORD):
     LIST_1 = word
@@ -52,11 +82,16 @@ def remove_stopwords(word, STOP_WORD):
             LIST_1.remove(w_1)
     return LIST_1
 
+
 def print_search_index(index):
+    '''
+        print the search index
+    '''
 
     keys = sorted(index.keys())
     for key in keys:
         print(key, " - ", index[key])
+
 
 def main():
     '''
