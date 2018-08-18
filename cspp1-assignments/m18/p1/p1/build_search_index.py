d9b26960b71de521fd09c5511f6492054b449fa4
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-18 12:53:08 +0530

    " -> 0 of 1 passed. pylint: 4.50/10 "

--- cspp1-assignments/m18/p1/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/p1/build_search_index.py
@@ -68,9 +68,9 @@ def build_search_index(docs):
         LIST_ = remove_stopwords(word_list(line), STOP_WORD)
     for word in set(LIST_):
          if word in dict_1:
-                dict_1[word].append((index_dict, LIST_.count(word)))
+                dict_1[word].append((index_dict_1, LIST_.count(word)))
          else:
-             dict_1[word] = [(index_dict, LIST_.count(word))]  
+             dict_1[word] = [(index_dict_1, LIST_.count(word))]  
 
     return dict_1
 
@@ -91,7 +91,6 @@ def print_search_index(index):
     keys = sorted(index.keys())
     for key in keys:
         print(key, " - ", index[key])
-
 
 def main():
     '''
