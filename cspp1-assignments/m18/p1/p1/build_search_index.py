734dba39c99a0f44c55c04c0a429f6b640ade3d4
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-18 12:16:33 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- cspp1-assignments/m18/p1/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/p1/build_search_index.py
@@ -1,107 +1,45 @@
-'''
-    Tiny Search Engine - Part 1 - Build a search index
-
-    In this programming assingment you are given with some text documents as input.
-    Complete the program below to build a search index. Don't worry, it is explained below.
-    A search index is a python dictionary.
-    The keys of this dictionary are words contained in ALL the input text documents.
-    The values are a list of documents such that the key/word appears in each document atleast once.
-    The document in the list is represented as a tuple.
-    The tuple has 2 items. The first item is the document ID.
-    Document ID is represented by the list index.
-    For example: the document ID of the third document in the list is 2
-    The second item of the tuple is the frequency of the word occuring in the document.
-
-    Here is the sample format of the dictionary.
-    {
-        word1: [(doc_id, frequency),(doc_id, frequency),...],
-        word2: [(doc_id, frequency),(doc_id, frequency),...],
-        .
-        .
-    }
-'''
-
-# helper function to load the stop words from a file
 import re
 def load_stopwords(filename):
-    '''
-        loads stop words from a file and returns a dictionary
-    '''
     stopwords = {}
     with open(filename, 'r') as f_stopwords:
         for line in f_stopwords:
             stopwords[line.strip()] = 0
     return stopwords
-
-
-def word_list(text):
-    '''
-        Change case to lower and split the words using a SPACE
-        Clean up the text by remvoing all the non alphabet characters
-        return a list of words
-    '''
+def word_list(string):
     regex = re.compile('[^a-z]')
     return [regex.sub("", w.strip()) for w in string.lower().split(" ")]
-  
 def build_search_index(docs):
-    '''
-        Process the docs step by step as given below
-    '''
-
-    # initialize a search index (an empty dictionary)
-
-    # iterate through all the docs
-    # keep track of doc_id which is the list index corresponding the document
-    # hint: use enumerate to obtain the list index in the for loop
-
-        # clean up doc and tokenize to words list
-
-        # add or update the words of the doc to the search index
-
-    # return search index
-    
-    dict_i = {}
+    dict_1 = {}
     STOP_WORD = load_stopwords("stopwords.txt")
     for index, line in enumerate(docs):
-        LIST_a = remove_stopwords(word_list(line), STOP_WORD)
-        for word in set(LIST_a):
-            if word in dict_i:
-                dict_i[word].append((index, LIST_a.count(word)))
+        LIST_ = remove_stopwords(word_list(line), STOP_WORD)
+        for ele in set(LIST_):
+            if ele in dict_1:
+                dict_1[ele].append((index, LIST_.count(ele)))
             else:
-                dict_i[word] = [(index, LIST_a.count(word))]
-    return dict_i
-# helper function to print the search index
-# use this to verify how the search index looks
+                dict_1[ele] = [(index, LIST_.count(ele))]
+    return dict_1
 
 def remove_stopwords(word, STOP_WORD):
-    LIST_i = word
+    LIST_1 = word
     for w_1 in word:
         if w_1 in STOP_WORD:
-            LIST_i.remove(w_1)
-    return LIST_i
+            LIST_1.remove(w_1)
+    return LIST_1
 def print_search_index(index):
-    '''
-        print the search index
-    '''
-    keys = sorted(index.keys())
-    for key in keys:
-        print(key, " - ", index[key])
-
-# main function that loads the docs from files
+keys = sorted(index.keys())
+for key in keys:
+    print(key, " - ", index[key])
 def main():
     '''
         main function
     '''
-    # empty document list
     documents = []
-    # iterate for n times
     lines = int(input())
-    # iterate through N times and add documents to the list
     for i in range(lines):
         documents.append(input())
         i += 1
 
-    # call print to display the search index
     print_search_index(build_search_index(documents))
 
 if __name__ == '__main__':
