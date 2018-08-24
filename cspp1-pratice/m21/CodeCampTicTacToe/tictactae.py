4c40357291610c0e380e842d9e8a25ef5de8addb
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-24 18:58:13 +0530

    " -> 14 of 14 passed. pylint: 8.57/10 "

--- cspp1-pratice/m21/CodeCampTicTacToe/tictactae.py
+++ cspp1-pratice/m21/CodeCampTicTacToe/tictactae.py
@@ -1,4 +1,11 @@
+'''
+
+author :@gvnaakhilsurya
+'''
 def is_win_tic_tac(matrix):
+    '''
+it gives the output coditions
+    '''
     if matrix[0][0] == matrix[1][1] == matrix[2][2]:
         return matrix[0][0]
     elif matrix[0][2] == matrix[1][1] == matrix[2][0]:
@@ -12,42 +19,47 @@ def is_win_tic_tac(matrix):
     elif matrix[0][0] == matrix[0][1] == matrix[0][2]:
         return matrix[0][0]
     elif matrix[1][0] == matrix[1][1] == matrix[1][2]:
-        return matrix [1][0]
+        return matrix[1][0]
     elif matrix[2][0] == matrix[2][1] == matrix[2][2]:
         return matrix[2][0]
     else:
         return ("invalid input")
-
 def is_validation(matrix):
+    '''
+   It checks for the condition
+    '''
     tic1_list = full_string(matrix)
-    if tic1_list.count('x') > 5 or  tic1_list.count('o') > 5  or  tic1_list.count('x')== tic1_list.count('o') : 
+    if tic1_list.count('x') > 5 or  tic1_list.count('o') > 5  or  tic1_list.count('x') == tic1_list.count('o'):
         return "invalid game"
-    for i in range(len(tic1_list)):
+    for char in range(len(tic1_list)):
         for j in tic1_list:
             if j not in 'ox.':
                 return "invalid input"
-    if(tic1_list.count('x')== 4 and tic1_list.count('o')==5 ) or (tic1_list.count('x')== 5 and tic1_list.count('o')==4 ):
+    if (tic1_list.count('x') == 4 and tic1_list.count('o') == 5) or (tic1_list.count('x') == 5 and tic1_list.count('o') == 4):
         return "draw"
 
     return 1
-
-
 def empty_tictac():
+    '''
     # it converts the input into lists
+    '''
     matrix = []
-    for i in range(3):
+    for num in range(3):
         list_temp = input().split()
         matrix.append(list_temp)
     return matrix
-
 def full_string(matrix):
-    l = []
+    '''
+    it converts thr lists of list into string
+    '''
+    list_temp = []
     for i in matrix:
-        l.extend(i)
-    return l
-
-
+        list_temp.extend(i)
+    return list_temp
 def main():
+    '''
+    it is the main function
+    '''
     inp_tic = empty_tictac()
     clean_string = full_string(inp_tic)
     output = is_validation(clean_string)
@@ -59,4 +71,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
