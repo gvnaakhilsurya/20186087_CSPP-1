9d376801d60da250c50954d22812ff1cf01f02a9
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-24 14:09:59 +0530

    " -> 4 of 14 passed. pylint: 5.38/10 "

--- cspp1-pratice/m21/CodeCampTicTacToe/tictactae.py
+++ cspp1-pratice/m21/CodeCampTicTacToe/tictactae.py
@@ -2,7 +2,7 @@
 
 def is_validation(n):
     tic1_list = full_string(n)
-    if tic1_list.count('x') > 5 or  tic1_list.count('o') > 5 :
+    if tic1_list.count('x') > 5 or  tic1_list.count('o') > 5 or tic1_list.count('x')==  tic1_list.count('o') > 5:
         return "invalid game"
     for i in range(len(tic1_list)):
         for j in tic1_list:
