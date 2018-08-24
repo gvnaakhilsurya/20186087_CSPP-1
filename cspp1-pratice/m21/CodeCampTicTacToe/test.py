7ea7536f097ab269f823854de2e73afc2676e80b
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-24 13:58:07 +0530

    " -> 0 of 14 passed. pylint: 3.46/10 "

--- cspp1-pratice/m21/CodeCampTicTacToe/test.py
+++ cspp1-pratice/m21/CodeCampTicTacToe/test.py
@@ -1,3 +1,17 @@
-a =['o . x . o . . x o']
-b=a[0].count('x')
-print(b)
+def is_validation(n):
+    tic1_list = n
+    if tic1_list.count('x') > 5 or  tic1_list.count('o') > 5 :
+        return "invalid game"
+    for i in range(len(tic1_list)):
+        for j in tic1_list[0]:
+            if j not in 'ox.':
+                return "invalid input"
+    return 1
+
+
+def main():
+    x = ['x', 'x', 'x', 'x', 'o', 'x', 'x', 'x', 'x']
+    print(is_validation(x))
+
+       
+main()
--- cspp1-pratice/m21/CodeCampTicTacToe/tictactae.py
+++ cspp1-pratice/m21/CodeCampTicTacToe/tictactae.py
@@ -5,7 +5,7 @@ def is_validation(n):
     if tic1_list.count('x') > 5 or  tic1_list.count('o') > 5 :
         return "invalid game"
     for i in range(len(tic1_list)):
-        for j in tic1_list[0]:
+        for j in tic1_list:
             if j not in 'ox.':
                 return "invalid input"
     return 1
