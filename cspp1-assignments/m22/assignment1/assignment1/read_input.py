0f1c6294b0c8e876d8d2538be6fa6bcde01ad499
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-08-25 11:27:50 +0530

    " -> 0 of 2 passed. pylint: 0.00/10 "

--- cspp1-assignments/m22/assignment1/assignment1/read_input.py
+++ cspp1-assignments/m22/assignment1/assignment1/read_input.py
@@ -3,7 +3,14 @@ Write a python program to read multiple lines of text input and store the input 
 '''
 
 def main():
-    pass
-
+	string = ' '
+	lines = int(input())
+	for i in range(lines):
+		i += 1
+		string = input()
+		string += '\n'
+	print(string)
+    
+     
 if __name__ == '__main__':
     main()
---
+++ cspp1-assignments/m22/assignment1/assignment1/test.py
@@ -0,0 +1,6 @@
+string = ''
+lines = int(input())
+    for i in range(lines):
+        i += 1
+        string += input()
+        string += '\n'
