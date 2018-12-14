a622d00d32c32fe2e3ca90aea4cc75545461d5c8
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-12-14 17:39:49 +0530

    " -> 4 of 4 passed. pylint: 10.00/10 "

--- cspp1-assignments/m4/p1/p1/vowels_counter.py
+++ cspp1-assignments/m4/p1/p1/vowels_counter.py
@@ -4,8 +4,7 @@ Write a program that counts up the number of vowels contained in the string s
 Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example,
 if s = 'azcbobobegghakl', your program should print:
 Number of vowels: 5
-@author : gvnaakhil
-this is the python problem
+@author :gvnaakhilsurya
 '''
 def main():
     '''
