0c8f792258fe251e7e306202ba0d4c2b14751dcc
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-12-20 12:39:18 +0530

    " -> 9 of 9 passed. pylint: 9.38/10 "

--- cspp1-assignments/m11/p1/p1/assignment1.py
+++ cspp1-assignments/m11/p1/p1/assignment1.py
@@ -12,17 +12,6 @@ SCRABBLE_LETTER_VALUES = {
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 def get_word_score(word, n_num):
-    """
-    Returns the score for a word. Assumes the word is a valid word.
-    The score for a word is the sum of the points for letters in the
-    word, multiplied by the length of the word, PLUS 50 points if all n
-    letters are used on the first turn.
-    Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
-    worth 3, D is worth 2, E is worth 1, and so on (see SCRABBLE_LETTER_VALUES)
-    word: string (lowercase letters)
-    n: integer (HAND_SIZE; i.e., hand size required for additional points)
-    returns: int >= 0
-    """
     sum_points = 0
     for i in word:
         if i in SCRABBLE_LETTER_VALUES:
