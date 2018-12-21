a55dfdabd58f28fc686a4ccf9d552a6892f3badb
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-12-21 10:15:04 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

--- remideal/CSPP-1_Question/Sudoko.py
+++ remideal/CSPP-1_Question/Sudoko.py
@@ -1,19 +1,15 @@
-# def check_duplicates(line):
-# 	chars = ".0123456789"
-# 	count = 0
-# 	if "." not  in line:
-# 		for i in line:
-# 			count = line.count(i)
-# 			if(count == 9):
-# 				print("Given sudoku is solved")
-# 			elif:
-# 				print("duplicates")
-# 	return 0
-# 	
-
+def check_duplicates(line):
+	chars = ".0123456789"
+	count = 0
+	if "." not  in line:
+		for i in line:
+			count = line.count(i)
+			if(count > 9):
+				return False
+	return True
 def length_verify(line):
 	count = 0
-	if(len(line)== 81):
+	if(len(line)== 81& check_duplicates(line)):
 		print("Given sudoku is solved")
 def main():
 	line = input()
--- remideal/CSPP-1_Question/test.py
+++ remideal/CSPP-1_Question/test.py
@@ -1,3 +1,13 @@
+def check_duplicates(line):
+	chars = ".0123456789"
+	count = 0
+	if "." not  in line:
+		for i in line:
+			count = line.count(i)
+			if(count > 9):
+				return False
+	return True
+
 def main():
 
 	s = "215647398368952174794381652586274931142593867973816425821739146659428.13437165281"
