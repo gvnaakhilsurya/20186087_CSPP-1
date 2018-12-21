6c945922a5b3b291c5a86503401dbdbd584a0a69
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-12-21 11:00:24 +0530

    "cspp1_m10_a1_1: 1/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 4/10 "

--- remideal/CSPP-1_Question/Sudoko.py
+++ remideal/CSPP-1_Question/Sudoko.py
@@ -1,18 +1,30 @@
+
 def check_duplicates(line):
-	chars = ".0123456789"
-	count = 0
-	if "." not  in line:
-		for i in line:
-			count = line.count(i)
-			if(count >= 9):
-				return False
-	return True
+    count = 0
+    if "." not  in line:
+        for i in line:
+            count = line.count(i)
+            if(count <= 9):
+                print("Given sudoku is solved")
+                break
+            else:
+                print("Invalid Sudoku:Duplicate values")
+
+
 def length_verify(line):
-	count = 0
-	if(len(line)== 81& check_duplicates(line)):
-		print("Given sudoku is solved")
+    if(len(line)== 81):
+        check_duplicates(line)
+    else:
+        if(len(line)< 81):
+            print("Invalid Input")
+
+
+
+
+
 def main():
-	line = input()
-	length_verify(line)
+    line = input()
+    length_verify(line)
+    # check_duplicates(line)
 if __name__ == '__main__':
-	main()
+    main()
--- remideal/CSPP-1_Question/test.py
+++ remideal/CSPP-1_Question/test.py
@@ -1,26 +1,18 @@
-def check_duplicates(line):
-	chars = ".0123456789"
-	count = 0
-	if "." not  in line:
-		for i in line:
-			count = line.count(i)
-			if(count > 9):
-				return False
-	return True
+
 
 def main():
+	s = "726493815315728946489651237852147693673985124941362758194836572567214389238579461"
+	count = 0
+	if "." not  in s:
+		for i in s:
+			count = s.count(i)
+			if(count <= 9):
+				print("comp")
+				break
+			else:
+				print("dpuli")
 
-	s = "215647398368952174794381652586274931142593867973816425821739146659428.13437165281"
-	print(len(s))
-	# for i in s:
-	# 	if "." in s:
-	# 		print("is their")
-# for i in line:
-# 		count = check_string.count(i)
-# 		if count >9:
-# 			print("duplicates are there")
-# 		elif:
-
+		
 
 if __name__ == '__main__':
 	main()
