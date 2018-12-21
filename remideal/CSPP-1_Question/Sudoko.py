f99e11af66bfa570989b27053fb9c3846ac982ca
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-12-21 09:42:53 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -17/10 "

---
+++ remideal/CSPP-1_Question/Sudoko.py
@@ -0,0 +1,10 @@
+def length_verify():
+	count = 0
+	if(len(s)==81):
+		print("Given sudoku is solved")
+		
+def main():
+	line = input()
+	length_verify(line)
+if __name__ == '__main__':
+	main()
---
+++ remideal/CSPP-1_Question/test.py
@@ -0,0 +1,13 @@
+def main():
+
+	s = "726493815315728946489651237852147693673985124941362758194836572567214389238579461"
+	print(len(s))
+# for i in line:
+# 		count = check_string.count(i)
+# 		if count >9:
+# 			print("duplicates are there")
+# 		elif:
+
+
+if __name__ == '__main__':
+	main()
